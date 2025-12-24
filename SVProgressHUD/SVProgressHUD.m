225f54b3afcc7049ae513e968b2941c1b0f4201b
Adrian Curtin <48138055+AdrianCurtin@users.noreply.github.com>
2025-12-24 14:02:45 -0500

    Fix HUD not displaying when containerView is no longer in hierarchy
    Check if containerView.window exists before using containerView.
    Falls back to frontWindow if containerView was deallocated.
    
    Fixes #1121

--- SVProgressHUD/SVProgressHUD.m
+++ SVProgressHUD/SVProgressHUD.m
@@ -542,7 +542,8 @@ static const CGFloat SVProgressHUDLabelSpacing = 8.0f;
 - (void)updateViewHierarchy {
     // Add the overlay to the application window if necessary
     if(!self.controlView.superview) {
-        if(self.containerView){
+        // Check if containerView is set and still in the view hierarchy
+        if(self.containerView && self.containerView.window){
             [self.containerView addSubview:self.controlView];
         } else {
 #if !defined(SV_APP_EXTENSIONS)
@@ -722,7 +723,7 @@ static const CGFloat SVProgressHUDLabelSpacing = 8.0f;
 
 - (void)moveToPoint:(CGPoint)newCenter rotateAngle:(CGFloat)angle {
     self.hudView.transform = CGAffineTransformMakeRotation(angle);
-    if (self.containerView) {
+    if (self.containerView && self.containerView.window) {
         self.hudView.center = CGPointMake(self.containerView.center.x + self.offsetFromCenter.horizontal, self.containerView.center.y + self.offsetFromCenter.vertical);
     } else {
         self.hudView.center = CGPointMake(newCenter.x + self.offsetFromCenter.horizontal, newCenter.y + self.offsetFromCenter.vertical);
